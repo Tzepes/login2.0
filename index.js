2a84b0e6c8e65dc736265b0050c80d74465b85bf
Tyras <andreicristian.vlad@gmail.com>
2018-07-15 15:52:47 +0200

    edited missing parameters error(shows only once now)

--- index.js
+++ index.js
@@ -6,9 +6,10 @@
 // document.getElementsByTagName('head')[0].appendChild(script); 
 
 var users = [];
-var counter=0;
 
 // $(document).ready(function(){
+
+    //Input bar effects
     $("input").focus(function(){
    		$(this).css("background-color", "#cccccc");
     });
@@ -16,12 +17,6 @@ var counter=0;
     $("input").blur(function(){
         $(this).css("background-color", "#ffffff");
     });
-
-    // $("button").hover(function(){
-    // 	$(this).css("border-color", "blue");
-    // },function(){
-    // 	$(this).css("border", "default");
-    // });
 
     /* 
         DONE||||1.Update form to use .submit() handler
@@ -32,40 +27,53 @@ var counter=0;
         the form
      */
 
+    //the function bellow contains the code for submiting the user input and storing it in the local storage 
     $("#loginForm").submit(function (event) {
-        event.preventDefault();
+        event.preventDefault(); //prevents page refresh after button press
 
+         //variables which takes every user input
         var fname = $("input#fname").val();
         var lname = $("input#lname").val();
         var email = $("input#email").val();
+        
 
+        userObj = {
+            firstname: fname,
+            lastname:lname,
+            email:email
+        }
+
+        //the function bellow checks if the user missed any parameters 
         $("#submit").one('click', function(){
             if($("input#fname").val() == 0 || $("input#lname").val() == 0 || $("input#email").val() == 0){    
                 $("input").css("border-color", "red");
                     
+                // finding the correct jquery method to replace the content of the div/element
+                $("ul#warn").replaceWith("<li style='color: red;'>Parameters missing</li>");
                 
-                $("ul#warn").append("<li style='color: red;'>Parameters missing</li>");
-                counter++;
-                
-
                 $("input").click(function(){    
                     $("input").css("border-color", "#ccc");
                 });
-            };
-        });
-    
-        $("form").find("input").each(function(){
-            users.push($(this).val());
+            } else { 
+                $("ul").remove("#warn");
+            }
         });
 
-        localStorage.setItem("firstname", fname);
-        localStorage.setItem("lastname", lname);
-        localStorage.setItem("email", email); 
 
-        var firstnameS = localStorage.getItem('firstname'),
-            lastnameS = localStorage.getItem('lastname'),
-            emailS = localStorage.getItem('email');
+        // takes every input vaule and pushes it into an array called users[]
+       users.push(userObj);
+        
+        
 
+        //sets every input value to local storage, mentions what value to take and it gives it a name
+        localStorage.setItem("user", userObj);
+
+        // vars for every value from the local storage, used for calling them later
+        var firstnameS = localStorage.getItem('user'),
+            lastnameS = localStorage.getItem('user'),
+            emailS = localStorage.getItem('user');
+
+        //shows all users from the local storage inside a table below the login form
         if($("input#fname").val() != 0 && $("input#lname").val() != 0 && $("input#email").val() != 0){
            $("table#loginInfo").append(
                     "<tr>" +
@@ -73,20 +81,17 @@ var counter=0;
                        "<th>" + lastnameS + "</th>" +
                        "<th>" + emailS + "</th>" +  
                     "</tr>"
-                       );
+                    );
             $("input").val(""); 
         }
-        
+    });
+    // showing and hiding table with users 
+    $("button#showUsers").click(function(){
+        $("table#loginInfo").show("slow");
+    });
 
-        var user = $(this).data({fname: fname, lname: lname, email: email});
-
-        $("button#showUsers").click(function(){
-            $("table#loginInfo").show("slow");
-        })
-
-        $("button#hideUsers").click(function(){
-            $("table#loginInfo").hide("slow");
-        })
+    $("button#hideUsers").click(function(){
+        $("table#loginInfo").hide("slow");
     });
 //});
 
